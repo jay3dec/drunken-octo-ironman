6491d9b84110c0f55b7818e8486245b2a921fc9b
jay <jay@jay-Inspiron-N4010.(none)>
2013-12-23 23:26:05 +0530

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+A simple tutorial on how to get started with visualizing data in 
+Angular js using Google Chart API
---
+++ app/js/controllers.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/* Controllers */
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(function () {
+    angular.bootstrap(document.body, ['myApp']);
+});
+angular.module('myApp.controllers', []).
+  controller('MyCtrl1', ['$scope',function($scope) {
+	var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2004',  1000,      400],
+          ['2005',  1170,      460],
+          ['2006',  660,       1120],
+          ['2007',  1030,      540]
+        ]);
+	var options = {
+          title: 'Company Performance'
+        };
+	var chart={};
+	chart.data = data;
+	chart.options = options;
+	$scope.chart = chart;
+        var chrt = new google.visualization.LineChart(document.getElementById('chartdiv'));
+        chrt.draw(data, options);
+  }])
+  .controller('MyCtrl2', [function() {
+
+  }]);
+
